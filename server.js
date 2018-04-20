520fa8c95e2195f47dc484b74c1056e0c28c2d29
aditiverma1127 <aditiverma1127@gmail.com>
2018-04-20 23:05:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/article-two',function(req,res) {
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
-app.get('/article-three',function(req,res) {
+app.get('/artcile-three',function(req,res) {
   res.sendFile(path.join(__dirname, 'ui', 'artcile-three.html'));
 });
 
