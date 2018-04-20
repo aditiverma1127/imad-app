f51372d95bc2d4d2f515c10c57d0f2315e906fc7
aditiverma1127 <aditiverma1127@gmail.com>
2018-04-20 23:58:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,14 +14,53 @@ var articleOne = {
                 This is my first content of artcile one.
                 
             </p>` 
+    };
     
-};
+    
+ function createTemplate(data) {
+     var title = data.title;
+     var heading = data.heading;
+     var date =data.date;
+     var content = data.content;
+ 
+    var htmlTemplate = `
+    <html>
+    <head>
+        <title>
+           ${title}
+        </title>
+        <meta name ="viewport" content="width=device-width,initial-scale=1"/>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>    
+    <body>
+        <div class ="container">
+        
+            <a href = "/"> Home </a>
+            
+            <hr/>
+            <h3>
+               ${heading}
+            </h3>
+            
+               ${date}
+            
+          ${content}
+            
+            </div>
+    </body>
+</html>
+`;
+    return  htmlTemplate;
+    
+  }  
+
+    
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req,res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(article-one));
 });
 
 app.get('/article-two',function(req,res) {
