3683901ed9ace8dc83a2050cbde84fc28bda1a05
aditiverma1127 <aditiverma1127@gmail.com>
2018-04-21 07:31:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,15 @@ element.innerHTML = 'NEW VALUE';
 
 //MOVE THE IMG
 var img = document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    marginLeft =marginLeft + 10;
+    //convert to string concate 10px
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function () {
+    //every 100milisec apply moveright function 
+    var interval = setInterval(moveRight,100);
     //move to right
     img.style.marginleft = '100px';
 };
