833d96f2308de8df991e610d0e58351b2d6aa1ba
aditiverma1127 <aditiverma1127@gmail.com>
2018-04-21 08:00:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,21 +1,10 @@
-console.log('Loaded!');
-//CHANGE THE element of main text div
-var element = document.getElementById('main-text');
-
-element.innerHTML = 'NEW VALUE';
-
-
-//MOVE THE IMG
-var img = document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft =marginLeft + 1;
-    //convert to string concate 10px
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function () {
-    //every 100milisec apply moveright function 
-    var interval = setInterval(moveRight,50);
-    //move to right
-    img.style.marginleft = '100px';
+// COUNTER CODE
+var button = document.getElementById('counter');
+button.onclick = function() {
+    //make req to counter end point
+    //capture response and kept in variable
+    //render the var in correct span
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
 };
