a7bbeff696e10213e8d18095e53f4cf209a48d56
aditiverma1127 <aditiverma1127@gmail.com>
2018-04-21 08:33:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,6 +25,21 @@ button.onclick = function() {
 request.open('GET','http://aditivermavccse15.imad.hasura-app.io/counter',true);
 request.send(null);
 
+};
 
-
+//submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+    var name = ['name1','name2','name3','name4'];
+//    convert into html string
+    var list ='';
+    for( var i =0; i< names.length;i++){
+        list += '<li>' + name[i] + '</li>';
+        
+    }
+    //insert html in ul
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
 };
