1e57fc0920684dbd7a167c5051a1d7f15ffbdbf3
aditiverma1127 <aditiverma1127@gmail.com>
2018-04-21 07:18:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,10 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML = 'NEW VALUE';
+
+
+//MOVE THE IMG
+var img = document.getElementById('img');
+img.onclick = function () {
+    img.style.marginleft = '100px';
+};
